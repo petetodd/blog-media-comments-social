707e1a2449b09246042a47c02d76ef281bc736ca
Peter Todd <peter@petertodd.com>
2013-03-01 11:00:37 +0000

    Starting Point
    A initial blog produced by working through the example discussed on the
    Home page in wiki

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogPeter1::Application.config.secret_token = '839c9f58541688c7cfe2b2df046f334c91b631aa033aad5b1fb2be4c8882fbb38752f62d5ac9b8ef161ab355a2bb6d2ed51962389782086705c982b18f124c00'
